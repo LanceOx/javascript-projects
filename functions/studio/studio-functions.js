6f31ff8e0e5db8d0c91d708b0440f99161ef649d
Lance Oxenfeld <oxenfeld13@gmail.com>
2024-01-25 20:34:38 -0600

    Functions Studio

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -1,7 +1,17 @@
 //We want to COMPLETELY reverse an array by flipping the order of the entries AND flipping the order of characters in each element.
 
 // Part One: Reverse Characters
-
+function reverseCharacters(str) {
+    let newstr = '';
+    if (typeof str === 'string'){
+    return str.split('').reverse().join('');
+} else if(typeof str === 'number'){
+    str = str.toString();
+    str.split('').reverse().join('');
+}
+return str.split('').reverse().join('');
+}
+console.log(reverseCharacters('Lance'))
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
 // 2. Within the function, split the string into an array, then reverse the array.
 // 3. Use join to create the reversed string and return that string from the function.
@@ -10,7 +20,21 @@
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
 // Part Two: Reverse Digits
+if (typeof reverseCharacters === 'string'){
+    return reverseCharacters
+} else if (typeof reverseCharacters === 'number'){
+    let reversedNumber = Number.toString().split('').reverse().join('');
+    return reversedNumber
+} 
+console.log(reverseCharacters(1234))
 
+// let number = 1234;
+// let stringNumber = number.toString();
+// let Reversal = stringNumber.split('').reverse().join('');
+// if (typeof str == 'string'){
+//     return reverseCharacters
+// } 
+// console.log(Reversal)
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
 // 2. If typeof is ‘string’, return the reversed string as before.
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
@@ -29,7 +53,12 @@
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+let emptyArr = [];
 
+for (i = 0; i < arrayTest1.length; i++){
+    reverseCharacters[arrayTest1]
+}
+ 
 // Bonus Missions
 
 // 1. Have a clear, descriptive name like funPhrase.
