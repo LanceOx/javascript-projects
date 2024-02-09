9c1b891413f5cf782e9a63f862a52e25fc987dc7
Lance Oxenfeld <oxenfeld13@gmail.com>
2024-02-09 09:41:54 -0600

    launchcode test studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,26 @@
-
 let launchcode = {
-  
+    organization: 'nonprofit',
+    executiveDirector: 'jeff',
+    percentageCoolEmployees: '100',
+    programsOffered: ['Web Developement', 'Data Analysis', 'Liftoff'],
+    launchOutput: function(n){
+        if(n/2===1){
+            return 'Launch!';
+        } 
+        if(n/3===1){
+            return 'code!';
+        }
+        if(n%5===0){
+            return 'rocks!';
+        }
+        if(n%6===0){
+            return 'LaunchCode!'
+        } 
+    }
+}; 
+
+function launchOutput(n){
+
 }
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,32 @@
-// launchcode.test.js code:
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("return 'nonprofit' as key value of organization", function(){
+    let keyval = launchcode.organization;
+    expect(keyval).toBe('nonprofit');
+  })
 
-  // Write your unit tests here!
+  test("return 'jeff' as a key value of executiveDirector", function(){
+    let dude = launchcode.executiveDirector;
+    expect(dude).toBe('jeff');
+  })
+
+  test("return '100' as a key value of percentageCoolEmployees", function(){
+    let employed = launchcode.percentageCoolEmployees;
+    expect(employed).toBe('100')
+  })
+
+  test("return ['Web Developement', 'Data Analysis', 'Liftoff'] as a key value for programsOffered", function(){
+    let array = launchcode.programsOffered;
+    expect(array).toStrictEqual(['Web Developement', 'Data Analysis', 'Liftoff'])
+  })
+
+  test('returns "Launch!" when divisible by 2', function(){
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+    expect(launchcode.launchOutput(3)).toBe('code!');
+    expect(launchcode.launchOutput(5)).toBe('rocks!');
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+  })
+
   
 });
